a53756f8d28ae4cceb907a88413265f6495acd91
CharLeff <93483174+CharLeff@users.noreply.github.com>
2021-10-31 10:57:22 -0500

    Thanks!

--- fo76.bat
+++ fo76.bat
@@ -1,4 +1,4 @@
 @echo off
-xcopy /i "D:\Program Files\ModifiableWindowsApps\Fallout 76\Project76.ini" "C:\Users\MYUSERNAME\OneDrive\Documents\My Games\Fallout 76\" /Y
-attrib +R "C:\Users\MYUSERNAME\OneDrive\Documents\My Games\Fallout 76\Project76.ini"
-start "" "C:\Users\MYUSERNAME\OneDrive\Desktop\Fallout 76.lnk"
+xcopy /i "D:\Program Files\ModifiableWindowsApps\Fallout 76\Project76.ini" "C:\Users\Charles Leffler\OneDrive\Documents\My Games\Fallout 76\" /Y
+attrib +R "C:\Users\Charles Leffler\OneDrive\Documents\My Games\Fallout 76\Project76.ini"
+start "" "C:\Users\Charles Leffler\OneDrive\Desktop\Fallout 76.lnk"
